09317b27c17b9eeeb99fb1fedb10c02af54c743d
anashamimi <anashamimi2@gmail.com>
2023-03-31 00:26:50 +0200

    aggiunta controllo se un prodotto è già presente nella lista

--- js/script.js
+++ js/script.js
@@ -7,43 +7,58 @@ per ogni elemento della lista aggiungere un btn cancella
 ed un btn con spunta
 */
 
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return{
+    data() {
+        return {
             listaSpesa: [
                 {
+                    quantity: 2,
                     name: 'pane',
                     completed: false
                 },
                 {
+                    quantity: 1,
                     name: 'acqua',
                     completed: false
                 }
             ],
+            newQuantity: 1,
             newLista: '',
             message: '',
         }
     },
     methods: {
-        inserisci(){
+        inserisci() {
             this.message = '';
-            const newItem= {
+            const newItem = {
+                quantity: this.newQuantity,
                 name: this.newLista,
                 completed: false
             }
-            if(this.newLista === ''){
+            if (this.newLista === '' || this.newQuantity < 1) {
                 this.message = 'inserisci un valore valido';
-            } else{
-                this.listaSpesa.push(newItem);                
+
+            } else {
+                let duplicate = false;
+                for (let i = 0; i < this.listaSpesa.length; i++) {
+                    if (this.listaSpesa[i].name === this.newLista) {
+                        duplicate = true;
+                    }
+                }
+                if (duplicate) {
+                    this.message = 'questo prodotto è già presente nella lista';
+                } else {
+                    this.listaSpesa.push(newItem);
+                }
             }
-            
+
         },
-        deleteList(index){
+        deleteList(index) {
             this.listaSpesa.splice(index, 1);
         },
-        complete(index){
+        complete(index) {
             this.listaSpesa[index].completed = true;
         }
     }
